8568d8d5eb9d2c3d98ff587e49a3780f48cfe56f
Jorge Vieira Alves <jorge.vieira.a@hotmail.com>
2020-08-28 07:06:53 -0300

    Projeto teste-web curso rapido iterasys

---
+++ ecommerce_prestashop/src/test/java/base/BaseTests.java
@@ -0,0 +1,56 @@
+package base;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.google.common.io.Files;
+
+import pages.HomePage;
+
+public class BaseTests {
+	
+	private static WebDriver driver;
+	protected HomePage homePage;
+	
+	@BeforeAll
+	public static void inicializar() {
+		System.setProperty("webdriver.chrome.driver", "/Users/jorgevieira/iterasys-workspace/drivers/Chrome/84/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);//qtd de tempo que o driver espera por um elemento quando ele nao esta presente
+
+		
+	}
+	
+	@BeforeEach
+	public void carregarPaginaInicial() {
+		driver.get("https://marcelodebittencourt.com/demoprestashop/");
+		homePage = new HomePage(driver);
+		
+	}
+	
+	public void capturarTela(String nomeTeste,String resultado) {
+		var camera = (TakesScreenshot) driver;
+		File capturaDeTela = camera.getScreenshotAs(OutputType.FILE);
+		try {
+			Files.move(capturaDeTela, new File("resources/Screenshots/"+nomeTeste+"_"+resultado+".png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+	@AfterAll
+	public static void finalizar() {
+		driver.quit();
+	}
+}
